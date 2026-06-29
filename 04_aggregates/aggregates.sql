4b6bec496839bb42b8cd6ea17203f970d96e539f
Gokul Krishnan <gokulkrishnan2004k@gmail.com>
2026-06-29 20:25:51 +0530

    Add aggregate functions for employee salaries
    Added SQL queries for aggregate functions on employees.

---
+++ 04_aggregates/aggregates.sql
@@ -0,0 +1,9 @@
+-- =========================================
+-- AGGREGATE FUNCTIONS
+-- =========================================
+
+SELECT COUNT(*) FROM employees;
+SELECT AVG(Salary) FROM employees;
+SELECT MAX(Salary) FROM employees;
+SELECT MIN(Salary) FROM employees;
+SELECT SUM(Salary) FROM employees;
